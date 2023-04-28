f76aa485aad7f043c1db791e0262b3991a5d356e
guummy <gmlee0505@gmail.com>
2023-04-28 19:47:20 +0900

    01

--- First/src/hello/Exercise1.java
+++ First/src/hello/Exercise1.java
@@ -4,7 +4,7 @@ public class Exercise1 {
 
 	public static void main(String[] args) {
 		// 연습문제 p.37 6번
-		System.out.println("내 이름은 이금미!");
+		System.out.println("내 이름은 이금미");
 	}
 
 }
--- First/src/hello/HelloJava.java
+++ First/src/hello/HelloJava.java
@@ -4,7 +4,7 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 //		System.out.println() : 화면에 출력하라는 뜻
-		System.out.println("Hello, Java");
+		System.out.println("Hello, Java!");
 	}
 
 }
